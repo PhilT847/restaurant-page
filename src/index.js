f7cbfb205833517b6e7822d8d39822fa8972696d
Philip Thomson <pst16@georgetown.edu>
2024-02-20 14:58:52 -0500

    Created page handler and page objects in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+// IIFE / module pattern for page handler
+const pageHandler = (function () {
+
+    const aboutPage = createAboutPage();
+    const menuPage = createMenuPage();
+    const contactPage = createContactPage();
+    
+});
+
+function createAboutPage() {
+    
+}
+
+function createMenuPage() {
+
+}
+
+function createContactPage() {
+
+}
