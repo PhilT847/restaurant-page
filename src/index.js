57f434a2486a588dfb70aa16cbaf1d2639663b9e
Philip Thomson <pst16@georgetown.edu>
2024-02-26 13:47:48 -0500

    index.js now creates nav buttons

--- src/index.js
+++ src/index.js
@@ -6,6 +6,35 @@ import { createContactTab } from "./contact-tab.js";
 const pageHandler = (function () {
 
     let mainContent = document.getElementById("content");
+    let navButtons = document.querySelector(".header-nav");
+
+    // Add buttons to nav
+    for(let i = 0; i < 3; i++) {
+
+        let navBtn = document.createElement("button");
+
+        // Nav button swaps to different tabs
+        navBtn.addEventListener("click", () => {
+            switchTab(i);
+        });
+
+        // Set class and text
+        navBtn.classList.add("header-button");
+
+        switch(i) {
+            case 0:
+                navBtn.textContent = "About";
+                break;
+            case 1:
+                navBtn.textContent = "Menu";
+                break;
+            case 2:
+                navBtn.textContent = "Contact";
+                break;
+        }
+
+        navButtons.appendChild(navBtn);
+    }
 
     // Set initial tab and open about tab
     let currentTab = 0;
