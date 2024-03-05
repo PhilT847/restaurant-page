be43dd4fa7118541c7591354172186770276edc7
Philip Thomson <pst16@georgetown.edu>
2024-03-05 13:53:39 -0500

    Split menu-tab into multiple menu-sections
    Adjusted menu-tab grid properties to fit multiple sections

--- src/menu-tab.js
+++ src/menu-tab.js
@@ -5,45 +5,67 @@ function createMenuTab() {
     tab.classList.add("menu-tab");
 
     // Add content
-    createItemCategory(tab, "Pizzas");
-    createItem(tab, "Small Pizza", 12.99);
-    createItem(tab, "Medium Pizza", 15.99);
-    createItem(tab, "Large Pizza", 18.99);
-    createItem(tab, "Add Toppings", "+1.99");
+    let currentSection = createItemSection(tab, "Pizzas");
+    createItem(currentSection, "Small Pizza", 12.99);
+    createItem(currentSection, "Medium Pizza", 15.99);
+    createItem(currentSection, "Large Pizza", 18.99);
+    createItem(currentSection, "Add Toppings", "+1.49");
+    createToppingOptions(currentSection);
+    
+    currentSection = createItemSection(tab, "Sides");
+    createItem(currentSection, "French Fries", 3.99);
+    createItem(currentSection, "Pizza Fries", 4.49);
+    createItem(currentSection, "Onion Rings", 3.99);
+    createItem(currentSection, "Chicken Tenders", 6.99);
 
-    createItemCategory(tab, "Sides");
-    createItem(tab, "French Fries", 3.99);
-    createItem(tab, "Pizza Fries", 4.49);
-    createItem(tab, "Onion Rings", 3.99);
-    createItem(tab, "Chicken Tenders", 6.99);
-
-    createItemCategory(tab, "Beverages");
-    createItem(tab, "2L Soda", 3.99);
-    createItem(tab, "Specialty Soda", 2.99);
-    createItem(tab, "Milkshake", 4.99);
+    currentSection = createItemSection(tab, "Beverages");
+    createItem(currentSection, "2L Soda", 3.99);
+    createItem(currentSection, "Specialty Soda", 2.99);
+    createItem(currentSection, "Milkshake", 4.99);
 
     return tab;
 }
 
-function createItemCategory(tab, name) {
+function createItemSection(tab, name) {
+
+    let section = document.createElement("div");
+    section.classList.add("menu-section");
 
     const categoryName = document.createElement("h2");
     categoryName.classList.add("menu-header");
     categoryName.textContent = name;
 
-    tab.appendChild(categoryName);
+    section.appendChild(categoryName);
+
+    tab.appendChild(section);
+
+    return section;
 }
 
 function createItem(tab, name, price) {
 
     const itemName = document.createElement("div");
     const itemPrice = document.createElement("div");
+    itemPrice.classList.add("menu-price");
 
     itemName.textContent = name;
     itemPrice.textContent = price;
 
     tab.appendChild(itemName);
     tab.appendChild(itemPrice);
+}
+
+function createToppingOptions(tab) {
+
+    const toppings = document.createElement("div");
+
+    toppings.textContent = "(Pepperoni, Sausage, Meatball, ";
+    toppings.textContent += "Ham, Peppers, Onions, ";
+    toppings.textContent += "Pineapple, Garlic)";
+
+    toppings.classList.add("menu-toppings");
+
+    tab.appendChild(toppings);
 }
 
 export {
