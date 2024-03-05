bc067fa0f6fc4082ea4c1d8e3fc8a7914a5ada57
Philip Thomson <pst16@georgetown.edu>
2024-03-05 15:02:00 -0500

    Filled about and contact tabs with dummy text

--- src/about-tab.js
+++ src/about-tab.js
@@ -1,9 +1,32 @@
 function createAboutTab() {
 
     const tab = document.createElement("div");
-    tab.textContent = "About";
+
     tab.classList.add("content-tab");
     tab.classList.add("about-tab");
+
+    // Header
+    const header = document.createElement("h2");
+    header.textContent = "About Us";
+    tab.appendChild(header);
+
+    // Text
+    const textOne = document.createElement("div");
+    textOne.textContent = "Lorem ipsum dolor sit amet, " + 
+                            "consectetur adipiscing elit, " +
+                            "sed do eiusmod tempor incididunt " +
+                            "ut labore et dolore magna aliqua. " +
+                            "Id eu nisl nunc mi ipsum faucibus " +
+                            "vitae. Egestas diam in arcu cursus " +
+                            "euismod quis viverra nibh cras.";
+    
+    const textTwo = document.createElement("div");
+    textTwo.classList.add("italics");
+    textTwo.textContent = "Augue neque gravida in fermentum et " +
+                            "sollicitudin ac orci phasellus!";
+
+    tab.appendChild(textOne);
+    tab.appendChild(textTwo);
 
     return tab;
 }
--- src/contact-tab.js
+++ src/contact-tab.js
@@ -1,11 +1,68 @@
 function createContactTab() {
 
     const tab = document.createElement("div");
-    tab.textContent = "Contact";
+
     tab.classList.add("content-tab");
     tab.classList.add("contact-tab");
 
+    // Header
+    const header = document.createElement("h2");
+    header.textContent = "Contact Us";
+    tab.appendChild(header);
+
+    // Add content
+    let currentSection = createContactSection(tab, "Matt Lorem");
+    createContactInfo(currentSection, 
+                        "Manager", 
+                        "123-456-7890", 
+                        "mlorem@lorempizza.com");
+
+    currentSection = createContactSection(tab, "Sherry Lorem");
+    createContactInfo(currentSection, 
+                        "Accounting", 
+                        "123-456-7891", 
+                        "slorem@lorempizza.com");
+
+    currentSection = createContactSection(tab, "Matt Lorem, Jr.");
+    createContactInfo(currentSection, 
+                        "Head Chef", 
+                        "123-456-7892", 
+                        "jlorem@lorempizza.com");
+
     return tab;
+}
+
+function createContactSection(tab, name) {
+
+    let section = document.createElement("div");
+    section.classList.add("contact-section");
+
+    const contactName = document.createElement("div");
+    contactName.classList.add("contact-header");
+    contactName.textContent = name;
+
+    section.appendChild(contactName);
+
+    tab.appendChild(section);
+
+    return section;
+}
+
+function createContactInfo(section, role, phone, email) {
+
+    let roleText = document.createElement("div");
+    roleText.textContent = role;
+    roleText.classList.add("italics");
+
+    let phoneText = document.createElement("div");
+    phoneText.textContent = "Phone: " + phone;
+
+    let emailText = document.createElement("div");
+    emailText.textContent = "Email: " + email;
+
+    section.appendChild(roleText);
+    section.appendChild(phoneText);
+    section.appendChild(emailText);
 }
 
 export {
